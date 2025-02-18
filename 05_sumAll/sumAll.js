91a0394dfc4ac461cc1d467dc719623845146c58
M4tys <147498089+M4tys@users.noreply.github.com>
2025-02-18 21:00:42 +0100

    Ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let sum = 0;
 
+    if(x < 0 || y < 0) return "ERROR";
+    if(!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR"; 
+
+    const first = Math.min(x,y);
+    const last = Math.max(x,y);
+
+    for(let i = first; i <= last; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
