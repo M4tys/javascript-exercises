12e81f9ec843c9dfc7648f2a071a9a6007349fbe
M4tys <147498089+M4tys@users.noreply.github.com>
2025-03-03 22:29:13 +0100

    Ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    var punctuation = /[\.,?! ]/g;
+    newText = text
+        .replace(punctuation, "")
+        .toLowerCase();
 
+    if(newText === newText.split("").reverse().join("")){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
