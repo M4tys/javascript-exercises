d73b28694c635209c674d5d75f698e0905884694
M4tys <147498089+M4tys@users.noreply.github.com>
2025-03-03 22:41:09 +0100

    Ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr = [0,1];
+    if(num < 0) return "OOPS"
+    for(let i = 2; i <= num; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
